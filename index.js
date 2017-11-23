1ff66e475303e3bc678aed6ea2beb9cb0c17771b
Kathy Hudson <khitx@icloud.com>
2017-11-23 15:59:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,3 @@ function doToElementsInArray(array, callback) {
 function changeCompletely(element, index, array) {
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
- 
